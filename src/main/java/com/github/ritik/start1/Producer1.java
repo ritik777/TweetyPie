1c5af16eae41dbef6613057c69f82042db9bf6ba
ritik <kitri@DESKTOP-GNPDQT7>
2020-07-03 02:02:13 -0400

    producer properties

--- src/main/java/com/github/ritik/start1/Producer1.java
+++ src/main/java/com/github/ritik/start1/Producer1.java
@@ -1,8 +1,36 @@
 package com.github.ritik.start1;
 
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.util.Properties;
+
 public class Producer1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+
+//Setting producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+//producer
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String,String> record = new ProducerRecord<String, String>( "first_topic", "Hello World");
+
+//        producer.send(record, new Callback() {
+//            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+//                if(e!= null){
+//
+//                }
+//            }
+//        })
+        producer.flush();
+        producer.close();
     }
 
 }
