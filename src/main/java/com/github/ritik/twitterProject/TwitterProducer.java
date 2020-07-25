4b33067f19b5764646fa76d8639511e445f9a9a7
ritik <kitri@DESKTOP-GNPDQT7>
2020-07-24 23:56:24 -0400

    Twitter client complete. ingested tweets successfully.

---
+++ src/main/java/com/github/ritik/twitterProject/TwitterProducer.java
@@ -0,0 +1,94 @@
+package com.github.ritik.twitterProject;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterProducer {
+
+    Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+    public TwitterProducer(){}
+
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+
+    }
+
+    public void run(){
+        //twitter client
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+        Client client = createTwitterClient(msgQueue);
+        // Attempts to establish a connection.
+        client.connect();
+
+        // kafka producer
+
+
+        //loop to send tweets to kafka
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if(msg!=null){
+                logger.info(msg);
+
+            }
+            //logger.info("End of Application!");
+
+        }
+        logger.info("End of Application!");
+
+    }
+
+    String consumerKey = "ziv5Vmxl6BuYaN4NggLRAqzql";
+    String consumerSecret = "FMbTKlnUV0DL9EISBXY96tQj4kTld8L7pBJz3ODcGuFIZjZPnk";
+    String token = "1286038885739499521-08fwtdwfq14zJ2dNrYOegKVCiT6Nff";
+    String secret= "ukz3BzAybyhzWGJQmzu65dQ7nrucOByKIeZ2PTYP7gdx7";
+
+    public Client createTwitterClient(BlockingQueue<String> msgQueue){
+
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+// Optional: set up some followings and track terms
+       // List<Long> followings = Lists.newArrayList(1234L, 566788L);
+        List<String> terms = Lists.newArrayList("bitcoin");
+        //hosebirdEndpoint.followings(followings);
+        hosebirdEndpoint.trackTerms(terms);
+
+// These secrets should be read from a config file
+        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+
+        Client hosebirdClient = builder.build();
+        return hosebirdClient;
+
+    }
+}
